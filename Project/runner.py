54f3cc311a34cd17e6ce9af6baac203f934bef10
avashachhami <avashachhamiii@gmail.com>
2026-02-06 11:34:28 +0545

    furtherTrainingRunner.py

--- Project/runner.py
+++ Project/runner.py
@@ -4,8 +4,9 @@ qCode.Game()
 qCode.Agent()
 
 learnTest1=qCode.Agent()
-qCode.Agent.train(learnTest1)
+qCode.Agent.train(learnTest1,25000)
 '''
 snake=qCode.Game()
 qCode.Game.runGame(snake,500)
 '''
+
