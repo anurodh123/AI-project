8197c57b7a956c9cdeefe2ab39ac06089a2462b1
Knull Infinite <nulfinity@gmail.com>
2026-02-07 22:56:46 +0545

    running past 25000 sloppily
    hard coded 25000 file instead of allowing user to input desired training start point, score average also looks weird but the snake does get better, i got a score of 64 with 25000 and 72 with 255000 even though printed updates say the average score for model is 2.3

--- Project/runner.py
+++ Project/runner.py
@@ -4,9 +4,10 @@ qCode.Game()
 qCode.Agent()
 
 learnTest1=qCode.Agent()
-qCode.Agent.train(learnTest1,25000)
+qCode.Agent.train(learnTest1)
 '''
 snake=qCode.Game()
 qCode.Game.runGame(snake,500)
 '''
+
 
